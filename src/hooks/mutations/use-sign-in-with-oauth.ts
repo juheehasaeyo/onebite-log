d04a2f503957c8e6c8d8f97434d0f4016583573e
조주희 <carla_cho@naver.com>
2026-02-04 21:40:55 +0900

    ~6.7 인증 에러 처리하기

--- src/hooks/mutations/use-sign-in-with-oauth.ts
+++ src/hooks/mutations/use-sign-in-with-oauth.ts
@@ -1,8 +1,12 @@
 import { signInWithOQuth } from "@/api/auth";
+import type { UseMutationCallback } from "@/types";
 import { useMutation } from "@tanstack/react-query";
 
-export function useSignInWithOAuth() {
+export function useSignInWithOAuth(callbacks?: UseMutationCallback) {
   return useMutation({
     mutationFn: signInWithOQuth,
+    onError: (error) => {
+      if (callbacks?.onError) callbacks.onError(error);
+    },
   });
 }
