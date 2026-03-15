dfb813240769fa540dde227757100daafe1dd682
조주희 <carla_cho@naver.com>
2026-03-15 22:28:45 +0900

    ~9.1 프로필 페이지 구현하기

--- src/hooks/queries/use-infinite-posts-data.ts
+++ src/hooks/queries/use-infinite-posts-data.ts
@@ -5,17 +5,24 @@ import { useInfiniteQuery, useQueryClient } from "@tanstack/react-query";
 
 const PAGE_SIZE = 5;
 
-export function useInfinitePostsData() {
+export function useInfinitePostsData(authorId?: string) {
   const quertClient = useQueryClient();
   const session = useSession();
 
   return useInfiniteQuery({
-    queryKey: QUERY_KEYS.post.list,
+    queryKey: !authorId
+      ? QUERY_KEYS.post.list
+      : QUERY_KEYS.post.userList(authorId),
     queryFn: async ({ pageParam }) => {
       const from = pageParam * PAGE_SIZE;
       const to = from + PAGE_SIZE - 1;
 
-      const posts = await fetchPosts({ from, to, userId: session!.user.id });
+      const posts = await fetchPosts({
+        from,
+        to,
+        userId: session!.user.id,
+        authorId,
+      });
       posts.forEach((post) => {
         quertClient.setQueryData(QUERY_KEYS.post.byId(post.id), post);
       });
