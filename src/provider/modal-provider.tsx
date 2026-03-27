b8f773769962dc60b8df4cb50d9103e353b6ce65
조주희 <carla_cho@naver.com>
2026-03-27 23:57:30 +0900

    ~9.4 프로필 수정 기능 구현하기 - 기능

--- src/provider/modal-provider.tsx
+++ src/provider/modal-provider.tsx
@@ -1,6 +1,6 @@
 import AlertModal from "@/components/modal/alert-modal";
 import PostEditorModal from "@/components/modal/post-editor-modal";
-import ProfileEditorModal from "@/components/modal/profile-deitor-modal";
+import ProfileEditorModal from "@/components/modal/profile-editor-modal";
 import type { ReactNode } from "react";
 import { createPortal } from "react-dom";
 
